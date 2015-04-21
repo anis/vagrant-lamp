6eecdcce7f1130fa52dc24d710f90c89af7938aa
Anis Safine <anis@safine.me>
2015-04-21 11:40:23 +0200

    Add mongo extension to PHP

--- puppet/manifests/database.pp
+++ puppet/manifests/database.pp
@@ -1,1 +1,12 @@
+# MongoDB
 include '::mongodb::server'
+php::pecl::module { "mongo":
+    use_package => "no",
+}
+
+php::ini { "php_ini_mongo":
+    value   => [
+        "extension=mongo.so"
+    ],
+    require => Class["php"]
+}
